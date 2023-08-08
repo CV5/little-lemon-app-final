3fe9ea79a7619d2714dba68ba451f36e85f86129
Cv5 <mrchrist85@gmail.com>
2023-08-08 17:02:58 -0400

    Initial Commit

---
+++ Little Lemon App Final/Little_Lemon_App_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_App_FinalApp.swift
+//  Little Lemon App Final
+//
+//  Created by Cristian  Veras on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_App_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
